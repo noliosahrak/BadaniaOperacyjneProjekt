4deebe7fae1c08cb059aa8f65e3dcbfde4194937
noliosahrak <wsburghardt@gmail.com>
2020-01-16 21:18:21 +0100

    Optymalizacja 2-opt, krok po kroku i natychmiastowa

--- JavaApplication2/src/javaapplication2/TymczasoweTesty.java
+++ JavaApplication2/src/javaapplication2/TymczasoweTesty.java
@@ -12,7 +12,7 @@ package javaapplication2;
 public class TymczasoweTesty {
     public static void testy() {
         
-        WektorRozwiazania wektor1 = new WektorRozwiazania(5);
+        //WektorRozwiazania wektor1 = new WektorRozwiazania(5);
         //WektorRozwiazania wektor2 = new WektorRozwiazania(10);
         
         //WektorRozwiazania wektor4 = new WektorRozwiazania(10);
@@ -24,13 +24,13 @@ public class TymczasoweTesty {
         //wektor3.wypiszRozwiazanie(); 
         //wektor4.wypiszRozwiazanie();
         
-        int[][] macierz = {{0,   341, 584, 486, 304},
+        /*int[][] macierz = {{0,   341, 584, 486, 304},
                            {341, 0,   299, 341, 304},
                            {584, 299, 0,   304, 403},
                            {486, 341, 304, 0,   280},
                            {304, 304, 403, 280, 0}};
-        String[] miasta = {"Gdańsk","Warszawa","Kraków","Wrocław","Poznań"};
-        MacierzOdleglosci mo = WczytywanieCSV.wczytajMacierzOdleglosciCSV("Macierz2.csv");
+        String[] miasta = {"Gdańsk","Warszawa","Kraków","Wrocław","Poznań"};*/
+        MacierzOdleglosci mo = WczytywanieCSV.wczytajMacierzOdleglosciCSV("Macierz3.csv");
         /*System.out.println(mo.wypiszTrase(wektor1));
         System.out.println(mo.obliczTrase(wektor1));  
         wektor1.losujRozwiazanie();
@@ -53,7 +53,7 @@ public class TymczasoweTesty {
         //WektorRozwiazania w2 = wektor1.mod2opt(0, 4);
         //w2.wypiszRozwiazanie();
         //System.out.println(mo.obliczTrase(w2));
-        WektorRozwiazania w3 = wektor1.mod2opt(0, 3);
+        WektorRozwiazania w3 = new WektorRozwiazania(mo.wymiarMacierzy());
         w3.losujRozwiazanie();
         System.out.println(mo.wypiszTrase(w3));
         w3.wypiszRozwiazanie();
